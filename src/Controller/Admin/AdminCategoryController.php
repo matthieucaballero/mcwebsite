5f863ea02f701f4dfd9769060a7e056e62da0027
matthieucaballero <51957195+matthieucaballero@users.noreply.github.com>
2020-03-19 17:59:51 +0100

    L'admin encore

--- src/Controller/Admin/AdminCategoryController.php
+++ src/Controller/Admin/AdminCategoryController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminCategoryController extends AbstractController
@@ -29,4 +31,52 @@ class AdminCategoryController extends AbstractController
 
         return $this->render('admin/admin_category/index.html.twig', compact('categories'));
     }
+
+    /**
+     * @Route("/admin/category/create", name="admin.category.new")
+     */
+    public function new(Request $request)
+    {
+        
+        $category = new Category(); 
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($category);
+            $this->em->flush();
+            return $this->redirectToRoute('admin_category');
+        }
+
+        return $this->render('admin/admin_category/new.html.twig', [
+            'category' => $category,
+            'form' => $form->createView()
+        ]);
+        
+    }
+
+    /**
+     * @Route("/admin/category/{id}", name="admin.category.edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Category $category )
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Catégorie modifiée' );
+            return $this->redirectToRoute('admin_category');
+        }
+
+        return $this->render('admin/admin_category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView()
+        ]);
+
+    }
+
+
+
 }
