18bf61eb192af2236e6c79e8e1ce9008069965f6
matthieucaballero <51957195+matthieucaballero@users.noreply.github.com>
2020-03-26 18:31:25 +0100

    Le front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Item;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $categoryRepository;
+    private $itemRepository;
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->categoryRepository = $em->getRepository(Category :: class);
+        $this->itemRepository = $em->getRepository(Item :: class);
+    }
+
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', []);
+        $categories = $this->categoryRepository->findAll();
+        $items = $this->itemRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+            'items' => $items
+        ]);
     }
 }
